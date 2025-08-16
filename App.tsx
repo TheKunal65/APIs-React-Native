eeec9af80d109b5f4f03f7166f2eeb22abdb788e
unknown <kunalprajapat65@gmail.com>
2025-08-16 15:17:04 +0530

    Simple form in react native

--- App.tsx
+++ App.tsx
@@ -1,44 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { Text, View, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [display, setDisplay] = useState(false);
+  const [visible , setVisible] = useState(true);
+  const resetDisplay = ()=>{
+    setDisplay(false);
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <View
+      style={{
+        backgroundColor: '#FFF9D4',
+        flex: 1,
+        borderWidth: 10,
+        borderColor: 'lightpink',
+        borderRadius: 35,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          textAlignVertical: 'center',
+          textAlign: 'center',
+          marginTop: 100,
+          backgroundColor: 'lightpink',
+          color: 'yellow',
+          height: 50,
+          borderRadius: 50,
+          margin: 70,
+          borderWidth: 4,
+          borderColor: '#aa6f73',
+        }}
+      >
+        Form in RN
+      </Text>
+      <TextInput
+        style={styles.textBox}
+        placeholder={'Enter User Name:'}
+        onChangeText={setName}
+        value={name}
       />
+      <TextInput
+        style={styles.textBox}
+        placeholder={'Enter User Email:'}
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.textBox}
+        placeholder={'Enter User password:'}
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={visible}
+      />
+      <Button color={'#aa6f73'} title="Clear" onPress={()=>resetDisplay()}/>
+      <Button color={'#aa6f73'} title="Submit" onPress={()=>setDisplay(true)}/>
+        <Button color={'#aa6f73'} title="See password" onPress={()=>setVisible(false)}/>
+      <View>
+        {
+          display ? 
+          <View>
+            <Text>User Name is: {name}</Text>
+            <Text>User Email is: {email}</Text>
+            <Text>User Password is: {password}</Text>
+          </View> : null
+        }
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  textBox: {
+    borderWidth: 2,
+    borderColor: '#aa6f73',
+    borderRadius: 20,
+    margin: 20,
   },
 });
 

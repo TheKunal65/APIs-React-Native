79c28a20bb1b4896e87af2a59a753b8725dcd32d
unknown <kunalprajapat65@gmail.com>
2025-09-09 22:31:23 +0530

    Design radio button from scratch

--- App.tsx
+++ App.tsx
@@ -1,88 +1,78 @@
-import { Text, View, TextInput, StyleSheet, Button } from 'react-native';
-import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [display, setDisplay] = useState(false);
-  const [visible , setVisible] = useState(true);
-  const resetDisplay = ()=>{
-    setDisplay(false);
-    setName("");
-    setEmail("");
-    setPassword("");
-  }
+  const [selectradio , setselectradio] = useState(2);
+
   return (
-    <View
-      style={{
-        backgroundColor: '#FFF9D4',
-        flex: 1,
-        borderWidth: 10,
-        borderColor: 'lightpink',
-        borderRadius: 35,
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 30,
-          textAlignVertical: 'center',
-          textAlign: 'center',
-          marginTop: 100,
-          backgroundColor: 'lightpink',
-          color: 'yellow',
-          height: 50,
-          borderRadius: 50,
-          margin: 70,
-          borderWidth: 4,
-          borderColor: '#aa6f73',
-        }}
-      >
-        Form in RN
-      </Text>
-      <TextInput
-        style={styles.textBox}
-        placeholder={'Enter User Name:'}
-        onChangeText={setName}
-        value={name}
-      />
-      <TextInput
-        style={styles.textBox}
-        placeholder={'Enter User Email:'}
-        onChangeText={setEmail}
-        value={email}
-      />
-      <TextInput
-        style={styles.textBox}
-        placeholder={'Enter User password:'}
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry={visible}
-      />
-      <Button color={'#aa6f73'} title="Clear" onPress={()=>resetDisplay()}/>
-      <Button color={'#aa6f73'} title="Submit" onPress={()=>setDisplay(true)}/>
-        <Button color={'#aa6f73'} title="See password" onPress={()=>setVisible(false)}/>
-      <View>
-        {
-          display ? 
-          <View>
-            <Text>User Name is: {name}</Text>
-            <Text>User Email is: {email}</Text>
-            <Text>User Password is: {password}</Text>
-          </View> : null
-        }
-      </View>
+    <View style={styles.main}>
+      <TouchableOpacity onPress={()=>setselectradio(1)}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {
+              selectradio==1 ? <View style={styles.radioBg}></View> : null
+              }
+          </View>
+          {selectradio==1 ? <Text style={[styles.radioText, styles.radioTextBorder]}>Radio 1</Text> : <Text style={styles.radioText}>Radio 1</Text>}
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=>setselectradio(2)}>
+        <View style={styles.radioWrapper}>
+          <View style={styles.radio}>
+            {
+              selectradio==2 ? <View style={styles.radioBg}></View> : null
+              }
+          </View>
+          {selectradio==2 ? <Text style={[styles.radioText, styles.radioTextBorder]}>Radio 2</Text> : <Text style={styles.radioText}>Radio 2</Text>}
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  textBox: {
-    borderWidth: 2,
-    borderColor: '#aa6f73',
-    borderRadius: 20,
-    margin: 20,
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  radioText: {
+    fontSize: 20,
+    color: 'skyblue',
+  },
+  radio: {
+    height: 40,
+    width: 40,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 20,
+    margin: 10,
+  },
+  radioWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  radioBg:{
+    backgroundColor:'skyblue',
+    height: 28,
+    width: 28,
+    borderRadius: 20,
+    margin: 4
+  },
+  radioTextBorder:{
+    borderWidth: 2,
+    borderColor: 'skyblue',
+    padding: 10,
+    backgroundColor: 'yellow',
+    borderRadius: 25,
+  }
 });
-
 export default App;

3cdcf4e5e6ebd3c10f7d0b69d97ea5e5367bbff0
unknown <kunalprajapat65@gmail.com>
2025-09-10 17:47:32 +0530

    Activity Indicator in RN

--- App.tsx
+++ App.tsx
@@ -5,54 +5,26 @@ import {
   StyleSheet,
   TouchableHighlight,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [selectradio, setselectradio] = useState(2);
-  const skills = [
-    {
-      id: 1,
-      name: 'C++',
-    },
-    {
-      id: 2,
-      name: 'JAVA',
-    },
-    {
-      id: 3,
-      name: 'DART',
-    },
-    {
-      id: 4,
-      name: 'Flutter',
-    },
-    {
-      id: 5,
-      name: 'React Native',
-    },
-  ];
-
+  const [show , setShow] = useState(false);
+  function displayLoader(){
+    setShow(true);
+    setTimeout(()=>{
+      setShow(false);
+    }, 3000);
+  }
   return (
     <View style={styles.main}>
-      {skills.map((item, index) => (
-        <TouchableOpacity onPress={() => setselectradio(item.id)}>
-          {' '}
-          key={index}
-          <View style={styles.radioWrapper}>
-            <View style={styles.radio}>
-              {selectradio == item.id ? <View style={styles.radioBg}></View> : null}
-            </View>
-            {selectradio == item.id ? (
-              <Text style={[styles.radioText, styles.radioTextBorder]}>
-                {item.name}
-              </Text>
-            ) : (
-              <Text style={styles.radioText}>{item.name}</Text>
-            )}
-          </View>
-        </TouchableOpacity>
-      ))}
+      <Text style={styles.text}>Activity Indicator</Text>
+      <ActivityIndicator size={100} color="gold" animating={show}/>
+      {
+        show ? <ActivityIndicator size="large" color="red"/> : null
+      }
+      <Button title="Click" onPress={(displayLoader)}/>
     </View>
   );
 }
@@ -63,36 +35,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  radioText: {
-    fontSize: 20,
-    color: 'skyblue',
-  },
-  radio: {
-    height: 40,
-    width: 40,
-    borderColor: 'black',
-    borderWidth: 2,
-    borderRadius: 20,
-    margin: 10,
-  },
-  radioWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  radioBg: {
-    backgroundColor: 'skyblue',
-    height: 28,
-    width: 28,
-    borderRadius: 20,
-    margin: 4,
-  },
-  radioTextBorder: {
-    borderWidth: 2,
-    borderColor: 'skyblue',
-    padding: 10,
-    backgroundColor: 'yellow',
-    borderRadius: 25,
+  text: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 50,
+    backgroundColor: 'lightblue',
+    borderRadius: 30,
+    margin: 25,
+    padding: 3,
+    color: 'yellow',
+    borderWidth: 5,
+    borderColor: 'purple',
+    fontWeight: 'bold',
   },
 });
+
 export default App;

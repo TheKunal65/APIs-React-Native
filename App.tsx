f28aa693a5bbb011b6b67d6ffdd83a76080940cb
unknown <kunalprajapat65@gmail.com>
2025-09-10 17:07:07 +0530

    Dynamicradio buttons using map functions and array of objects

--- App.tsx
+++ App.tsx
@@ -9,30 +9,50 @@ import {
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [selectradio , setselectradio] = useState(2);
+  const [selectradio, setselectradio] = useState(2);
+  const skills = [
+    {
+      id: 1,
+      name: 'C++',
+    },
+    {
+      id: 2,
+      name: 'JAVA',
+    },
+    {
+      id: 3,
+      name: 'DART',
+    },
+    {
+      id: 4,
+      name: 'Flutter',
+    },
+    {
+      id: 5,
+      name: 'React Native',
+    },
+  ];
 
   return (
     <View style={styles.main}>
-      <TouchableOpacity onPress={()=>setselectradio(1)}>
-        <View style={styles.radioWrapper}>
-          <View style={styles.radio}>
-            {
-              selectradio==1 ? <View style={styles.radioBg}></View> : null
-              }
+      {skills.map((item, index) => (
+        <TouchableOpacity onPress={() => setselectradio(item.id)}>
+          {' '}
+          key={index}
+          <View style={styles.radioWrapper}>
+            <View style={styles.radio}>
+              {selectradio == item.id ? <View style={styles.radioBg}></View> : null}
+            </View>
+            {selectradio == item.id ? (
+              <Text style={[styles.radioText, styles.radioTextBorder]}>
+                {item.name}
+              </Text>
+            ) : (
+              <Text style={styles.radioText}>{item.name}</Text>
+            )}
           </View>
-          {selectradio==1 ? <Text style={[styles.radioText, styles.radioTextBorder]}>Radio 1</Text> : <Text style={styles.radioText}>Radio 1</Text>}
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={()=>setselectradio(2)}>
-        <View style={styles.radioWrapper}>
-          <View style={styles.radio}>
-            {
-              selectradio==2 ? <View style={styles.radioBg}></View> : null
-              }
-          </View>
-          {selectradio==2 ? <Text style={[styles.radioText, styles.radioTextBorder]}>Radio 2</Text> : <Text style={styles.radioText}>Radio 2</Text>}
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
@@ -60,19 +80,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  radioBg:{
-    backgroundColor:'skyblue',
+  radioBg: {
+    backgroundColor: 'skyblue',
     height: 28,
     width: 28,
     borderRadius: 20,
-    margin: 4
+    margin: 4,
   },
-  radioTextBorder:{
+  radioTextBorder: {
     borderWidth: 2,
     borderColor: 'skyblue',
     padding: 10,
     backgroundColor: 'yellow',
     borderRadius: 25,
-  }
+  },
 });
 export default App;

491112b0ed30eeff43cb9e40eefcbcfaef4cf15a
unknown <kunalprajapat65@gmail.com>
2025-09-12 17:25:31 +0530

    Status bar in RN

--- App.tsx
+++ App.tsx
@@ -1,49 +1,25 @@
-import {
-  View,
-  Text,
-  Button,
-  StyleSheet,
-  TouchableHighlight,
-  TouchableOpacity,
-  ActivityIndicator,
-  Modal,
-  Pressable,
-} from 'react-native';
+import { View, Text,StyleSheet, StatusBar, Button } from 'react-native';
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [showStatusBar, setShowStatusBar] = useState(true);
+  const [barSty , setBarSty] = useState("default");
   return (
     <View style={styles.main}>
-      <Pressable 
-      onPress={() => console.log('OnPress')}
-      // long press ka delay default 500ms hota hai
-      onLongPress={()=> console.log('OnLongPress')}
-      onPressIn={()=> console.log('OnPressIn')}
-      onPressOut={()=>console.log('OnPressOut')}
-      >
-        <Text style={styles.text}>Pressable</Text>
-      </Pressable>
+      <StatusBar barStyle={barSty} hidden={showStatusBar} />
+      <Button title='Update StatusBar'onPress={()=>setShowStatusBar(!showStatusBar)}/>
+      <Button title='Update Style' onPress={()=>setBarSty("light-content")}/>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
-  main: {
+  main:{
     flex: 1,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 50,
-    borderWidth: 3,
-    margin: 95,
-    borderRadius: 30,
-    backgroundColor: '#889defff',
-    color: 'white',
-    shadowColor: 'black',
-    elevation: 10,
-  },
-});
+})
 
 export default App;

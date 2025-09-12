5045ba3d26a1f34e8f7ff091e0c4ac549d924cf6
unknown <kunalprajapat65@gmail.com>
2025-09-12 18:41:52 +0530

    How to emmbed website url in Mobile OS

--- App.tsx
+++ App.tsx
@@ -1,41 +1,13 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  StatusBar,
-  Button,
-  Platform,
-} from 'react-native';
+import { View, Text } from 'react-native';
 import React, { useState, useEffect } from 'react';
+import { WebView } from 'react-native-webview';
 
 function App() {
   return (
-    <View>
-      <Text style={{ fontSize: 40, fontWeight: 'bold' }}>
-        Platform= {Platform.OS}
-      </Text>
-      {Platform.OS == 'android' ? (
-        <View
-          style={{ height: 100, width: 100, backgroundColor: 'lightgreen' }}
-        ></View>
-      ) : (
-        <View
-          style={{ height: 100, width: 100, backgroundColor: 'red' }}
-        ></View>
-      )}
-
-      <Text style={styles.text}>Hello Kunal</Text>
-      <Text style={{fontSize: 20}}>{JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
-    </View>
+    <WebView
+      source={{ uri: 'https://www.linkedin.com/in/kunal-prajapat-487079263/' }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  text:{
-    color: Platform.OS=="android" ? "orange" : "blue",
-    fontWeight: 'bold',
-    fontSize: 40,
-  }
-})
 
 export default App;

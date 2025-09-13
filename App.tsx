7476587e1f987788f487e885cd46232078b4f0c1
unknown <kunalprajapat65@gmail.com>
2025-09-13 16:34:31 +0530

    customModal_aka_dialogBox

--- App.tsx
+++ App.tsx
@@ -1,13 +1,45 @@
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet, Button} from 'react-native';
 import React, { useState, useEffect } from 'react';
-import { WebView } from 'react-native-webview';
 
 function App() {
+  const [modalVisible , setShowModal] = useState(false);
   return (
-    <WebView
-      source={{ uri: 'https://www.linkedin.com/in/kunal-prajapat-487079263/' }}
-    />
+    <View style={styles.container}>
+      {
+        modalVisible ? <View style={styles.modal}>
+        <View style={styles.body}>
+          <Text>Some Text</Text>
+          <Button title="Close" onPress={()=>setShowModal(false)}/>
+        </View>
+      </View> : null
+      }
+      <Button title="Open Dialog" onPress={()=>setShowModal(true)}/>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'flex-end',
+    marginBottom: 50,
+  },
+  modal:{
+    alignItems:'center',
+    justifyContent:'center',
+    flex: 1,
+    backgroundColor: 'rgba(50,50,50,0.5)'
+  },
+  body:{
+    alignItems:'center',
+    justifyContent:'center',
+    height: 300,
+    width: 300,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 30,
+    borderWidth: 2,
+  }
+})
 
 export default App;

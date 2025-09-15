1df2e8b4515bd38e947005f6ee3e7c59ece04bb9
unknown <kunalprajapat65@gmail.com>
2025-09-15 13:50:34 +0530

    flatList_APIData

--- App.tsx
+++ App.tsx
@@ -1,61 +1,71 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import React, { useEffect, useState } from 'react';
 
 function App() {
   const [data, setdata] = useState([]);
+
   async function getAPIData() {
     const url = 'https://jsonplaceholder.typicode.com/posts';
     let result = await fetch(url);
     result = await result.json();
     setdata(result);
   }
+
   useEffect(() => {
     getAPIData();
   }, []);
 
   return (
-    <ScrollView>
-      <View style={styles.main}>
-        <Text style={styles.text}>Simple API Call</Text>
-        {data.length
-          ? data.map(item => (
-              <View
+    <View style={{ backgroundColor: 'peachpuff', flex: 1 }}>
+      <Text style={styles.text}>FlatList With API Data</Text>
+      {data.length ? (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                padding: 10,
+                backgroundColor: 'white',
+                borderBottomWidth: 1,
+                borderBottomColor: '#ccc',
+                margin: 10,
+                borderRadius: 25,
+                shadowColor: '#000',
+                elevation: 5,
+              }}
+            >
+              <Text
                 style={{
-                  padding: 20,
-                  borderBottomColor: 'grey',
-                  borderBottomWidth: 3,
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  backgroundColor: 'lightyellow',
+                  borderBlockColor: 'black',
+                  borderWidth: 1,
+                  padding: 5,
+                  marginBottom: 5,
+                  borderRadius: 20,
                 }}
               >
-                <Text
-                  style={{
-                    fontSize: 20,
-                    backgroundColor: 'lightyellow',
-                    borderRadius: 15,
-                  }}
-                >
-                  Id: {item.id}
-                </Text>
-                <Text style={{ fontSize: 20 }}>Title: {item.title}</Text>
-                <Text style={{ fontSize: 20 }}>Body: {item.body}</Text>
-              </View>
-            ))
-          : null}
-      </View>
-    </ScrollView>
+                {item.id}
+              </Text>
+              <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+                {item.title}
+              </Text>
+              <Text style={{ fontSize: 20 }}>{item.body}</Text>
+            </View>
+          )}
+        />
+      ) : null}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    backgroundColor: 'peachpuff',
-  },
   text: {
-    fontSize: 50,
-    textAlign: 'center',
+    fontSize: 30,
     fontWeight: 'bold',
+    textAlign: 'center',
     marginTop: 50,
-    color: 'maroon',
   },
 });
 

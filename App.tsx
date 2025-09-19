c117884e78b47cead5e72517b85f81a146621a5d
unknown <kunalprajapat65@gmail.com>
2025-09-19 17:35:23 +0530

    Making list of API data

--- App.tsx
+++ App.tsx
@@ -1,143 +1,87 @@
-import { View, Text, Button, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
 function App() {
-  const [name, setname] = useState('');
-  const [age, setage] = useState(0);
-  const [email, setemail] = useState('');
-
-  const [nameError, setnameError] = useState(false);
-  const [ageError, setageError] = useState(false);
-  const [emailError, setemailError] = useState(false);
-
-
-  async function saveAPIData() {
-
-    const data = {
-      name: name,
-      age: age,
-      email: email,
-    };
-
-    // validations using if else
-
-    // if(!name){
-    //   setnameError(true);
-    // }else{
-    //   setnameError(false);
-    // }
-    // if(!age){
-    //   setageError(true);
-    // }else{
-    //   setageError(false);
-    // }
-    // if(!email){
-    //   setemailError(true);;
-    // }else{
-    //   setemailError(false);
-    // }
-
-    // validations using ternary operator
-    !name ? setnameError(true) : setnameError(false);
-    !age ? setageError(true) : setageError(false);
-    !email ? setemailError(true) : setemailError(false);
-
-
-    // this condition is used to stop the function if any field is empty
-    if(!name || !age || !email){
-      return false;
+  const [data, setData] = useState([]);
+  async function getAPIData() {
+    const url = 'http://10.238.53.127:3000/users';
+    let result = await fetch(url);
+    result = await result.json();
+    if (result) {
+      setData(result);
     }
-
-    console.warn("Next")
-
-
-    const url = 'http://10.147.203.9:3000/users';
-    let result = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
-    (result = await result.json()), console.log(result);
   }
+  useEffect(() => {
+    getAPIData();
+  }, []);
 
   return (
-    <View style={styles.main}>
-      <Text style={{ fontSize: 40, fontWeight: 'bold', textAlign: 'center' }}>
-        POST API Call by using form
-      </Text>
-
-      <View
-        style={{
-          margin: 20,
-          padding: 20,
-          borderWidth: 2,
-          borderColor: 'maroon',
-          backgroundColor: 'lightyellow',
-          borderRadius: 30,
-        }}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Name:"
-          onChangeText={text => setname(text)}
-        />
-        {
-          nameError ? <Text style={styles.error}>*Please Enter Valid Name</Text> : null
-        }
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Age:"
-          onChangeText={text => setage(text)}
-        />
-        {
-          ageError ? <Text style={styles.error}>*Please Enter Valid Age</Text> : null
-        }
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Email:"
-          onChangeText={text => setemail(text)}
-        />
-        {
-          emailError ? <Text style={styles.error}>*Please Enter Valid Email</Text> : null
-        }
-        {/* <Button title="SUBMIT DATA" onPress={() => saveAPIData()}/> */}
-        <TouchableOpacity onPress={()=> saveAPIData()}>
-          <View style={styles.button}>
-            <Text style={{color: 'white', fontWeight: 'bold'}}>SUBMIT DATA</Text>
-          </View>
-        </TouchableOpacity>
+    <ScrollView>
+      <View style={styles.main}>
+        <Text style={styles.topic}>List with API Data</Text>
+        <View style={styles.dataWrapper}>
+        <View style={{ flex: 1.7}}>
+          <Text style={{fontWeight: 'bold' }}>Name</Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          <Text style={{fontWeight: 'bold' }}>Age</Text>
+        </View >
+        {/* <View style={{flex: 1}}><Text>{item.email}</Text></View> */}
+        <View style={{flex: 1}}><Text style={{fontWeight: 'bold' }}>Operations</Text></View>
+        </View>
+        {data.length
+          ? data.map(item => (
+              <View style={styles.dataWrapper}>
+                <View style={{ flex: 1.7 }}>
+                  <Text>{item.name}</Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                  <Text>{item.age}</Text>
+                </View>
+                {/* <View style={{flex: 1}}><Text>{item.email}</Text></View> */}
+                <View style={{ flex: 1 }}>
+                  <Button title="Update" />
+                </View>
+                <View style={{ flex: 1 }}>
+                  <Button title="Delete" />
+                </View>
+              </View>
+            ))
+          : null}
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   main: {
-    flex: 1,
     backgroundColor: 'peachpuff',
-    padding: 10,
-    alignContent: 'center',
-    justifyContent: 'center',
+    flex: 1,
   },
-  input: {
-    borderWidth: 1,
+  topic: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 60,
+    backgroundColor: 'lightyellow',
+    padding: 10,
     margin: 10,
-    marginBottom: 5,
-    padding: 10,
+    borderRadius: 30,
+    borderWidth: 2,
     borderColor: 'maroon',
-    borderRadius: 20,
+    borderStyle: 'solid',
   },
-  button: {
-    backgroundColor: 'maroon',
-    marginTop: 20,
+  dataWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: 'orange',
+    margin: 5,
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
     borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'brown',
+    borderStyle: 'dashed',
   },
-  error:{
-    color: 'red',
-    marginLeft: 20,
-  }
 });
 
 export default App;
